0957f015316e12a47e25d915e98dc94f0b9399b7
Abolmasow <zevssa1998@gmail.com>
2024-03-02 17:29:12 +0300

    func reserve

--- libs/data_structures/vector/vector.c
+++ libs/data_structures/vector/vector.c
@@ -12,3 +12,20 @@ vector create_vector(size_t capacity) {
         exit(1);
     }
 }
+
+void reserve(vector *v, size_t new_capacity) {
+    if (new_capacity > 0) {
+        int *new_data = realloc(v->data, new_capacity * sizeof(int));
+        if (new_data == NULL) {
+            fprintf(stderr, "bad alloc");
+            exit(1);
+        }
+        v->data = new_data;
+        v->capacity = new_capacity;
+        if (new_capacity < v->size) {
+            v->size = new_capacity;
+        }
+    } else {
+        delete_vector(v);
+    }
+}
