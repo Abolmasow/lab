3f2c5d3b9d9327b15fa91ba087693c72b042a43a
Abolmasow <zevssa1998@gmail.com>
2024-03-02 17:23:25 +0300

    creat lib vector

---
+++ libs/data_structures/vector/vector.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <malloc.h>
+#include <stdlib.h>
+#include "../../data_structures/vector/vector.h"
+
+vector create_vector(size_t capacity) {
+    if (capacity <= UINT64_MAX) {
+        return (vector) {malloc(sizeof(int) * capacity), 0, capacity};
+    } else {
+        fprintf(stderr, "bad alloc");
+        exit(1);
+    }
+}
