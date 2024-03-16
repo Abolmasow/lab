4c24320c0678ce64c3d365b20e61ed7bb994a239
Abolmasow <zevssa1998@gmail.com>
2024-03-16 18:20:06 +0300

    second append access functions

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -5,6 +5,12 @@
 #include <memory.h>
 #include "matrix.h"
 
+void swap(int* a, int* b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
 matrix getMemMatrix(int nRows, int nCols) {
     int **values = (int **) malloc(sizeof(int *) * nRows);
 
@@ -140,4 +146,153 @@ void selectionSortColsMatrixByColCriteria(matrix *m, int (*criteria)(int*, int))
 
         swapColumns(m, value_min_idx, i);
     }
+}
+
+bool isSquareMatrix(matrix *m) {
+    return m->nRows == m->nCols;
+}
+
+bool areTwoMatricesEqual(matrix *m1, matrix *m2) {
+    if (m1->nRows != m2->nRows || m1->nCols != m2->nCols) {
+        return false;
+    }
+
+    for (int i = 0; i < m1->nRows; i++) {
+        for (int j = 0; j < m1->nCols; j++) {
+            if (m1->values[i][j] != m2->values[i][j]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool isEMatrix(matrix *m) {
+    if (m->nRows != m->nCols) {
+        fprintf(stderr, "Is not square matrix");
+        exit(1);
+    }
+
+    for (int i = 0; i < m->nRows; i++) {
+        for (int j = 0; j < m->nCols; j++) {
+            if (i == j && m->values[i][j] != 1) {
+                return false;
+            } else if (i != j && m->values[i][j] != 0) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+bool isSymmetricMatrix(matrix *m) {
+    for (int i = 0; i < m->nRows; i++) {
+        for (int j = i + 1; j < m->nCols; j++) {
+            if (m->values[i][j] != m->values[j][i]) {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+
+void transposeSquareMatrix(matrix *m) {
+    if (m->nRows != m->nCols) {
+        fprintf(stderr, "Is not square matrix");
+        exit(1);
+    }
+
+    for (int i = 0; i < m->nRows; i++) {
+        for (int j = i + 1; j < m->nCols; j++) {
+            if (i != j) {
+                int temp = m->values[i][j];
+                m->values[i][j] = m->values[j][i];
+                m->values[j][i] = temp;
+            }
+        }
+    }
+}
+
+void transposeMatrix(matrix *m) {
+    int** new_value = (int**) malloc(sizeof(int*) * m->nCols);
+
+    for (int i = 0; i < m->nCols; i++) {
+        new_value[i] = (int*) malloc(sizeof(int) * m->nRows);
+
+        for (int j = 0; j < m->nRows; j++) {
+            new_value[i][j] = m->values[j][i];
+        }
+    }
+
+    for (int i = 0; i < m->nRows; i++)
+        free(m->values[i]);
+    free(m->values);
+
+    m->values = new_value;
+    swap(&m->nRows, &m->nCols);
+}
+
+position getMinValuePos(matrix m) {
+    int min_value = m.values[0][0];
+    position pos = {0, 0};
+
+    for (int i = 1; i < m.nRows; i++) {
+        for (int j = 1; j < m.nCols; j++) {
+            if (m.values[i][j] < min_value) {
+                min_value = m.values[i][j];
+                pos.rowIndex = i;
+                pos.colIndex = j;
+            }
+        }
+    }
+
+    return pos;
+}
+
+position getMaxValuePos(matrix m) {
+    int max_value = m.values[0][0];
+    position pos = {0, 0};
+
+    for (int i = 0; i < m.nRows; i++) {
+        for (int j = 0; j < m.nCols; j++) {
+            if (m.values[i][j] > max_value) {
+                max_value = m.values[i][j];
+                pos.rowIndex = i;
+                pos.colIndex = j;
+            }
+        }
+    }
+
+    return pos;
+}
+
+matrix createMatrixFromArray(const int a[], int nRows, int nCols) {
+    matrix m = getMemMatrix(nRows, nCols);
+
+    int k = 0;
+    for (int i = 0; i < nRows; i++) {
+        for (int j = 0; j < nCols; j++) {
+            m.values[i][j] = a[k++];
+        }
+    }
+
+    return m;
+}
+
+matrix* createArrayOfMatrixFromArray(const int values[], size_t nMatrices, size_t nRows, size_t nCols) {
+    matrix* ms = getMemArrayOfMatrices(nMatrices, nRows, nCols);
+
+    int l = 0;
+    for (size_t k = 0; k < nMatrices; k++) {
+        for (size_t i = 0; i < nRows; i++) {
+            for (size_t j = 0; j < nCols; j++) {
+                ms[k].values[i][j] = values[l++];
+            }
+        }
+    }
+
+    return ms;
 }
--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -63,4 +63,23 @@ bool isEMatrix(matrix *m);
 // возвращает значение ’истина’, если матрица m является симметричной, ложь – в противном случае.
 bool isSymmetricMatrix(matrix *m);
 
+// транспонирует квадратную матрицу m.
+void transposeSquareMatrix(matrix *m);
+
+// транспонирует матрицу m.
+void transposeMatrix(matrix *m);
+
+// возвращает позицию минимального элемента матрицы m.
+position getMinValuePos(matrix m);
+
+// возвращает позицию максимального элемента матрицы m.
+position getMaxValuePos(matrix m);
+
+// возвращает матрицу размера n_rows на n_cols, построенную из элементов массива a.
+matrix createMatrixFromArray(const int a[], int nRows, int nCols);
+
+// возвращает указатель на нулевую матрицу массива из n_matrices матриц, размещенных
+//в динамической памяти, построенных из элементов массива a.
+matrix* createArrayOfMatrixFromArray(const int values[], size_t nMatrices, size_t nRows, size_t nCols);
+
 #endif //UNTITLED6_MATRIX_H
