c81679b6fb56752d78b311b1673aae79b97e8a65
Abolmasow <zevssa1998@gmail.com>
2024-03-16 17:57:21 +0300

    append access functions

--- libs/data_structures/matrix/matrix.c
+++ libs/data_structures/matrix/matrix.c
@@ -43,3 +43,101 @@ void freeMemMatrices(matrix *ms, int nMatrices) {
     ms->nRows = 0;
     ms->nCols = 0;
 }
+
+void inputMatrix(matrix *m) {
+    for (int i = 0; i < m->nRows; i++) {
+        printf("input %d row:", i + 1);
+        for (int j = 0; j < m->nCols; j++) {
+            scanf("%d", &m->values[i][j]);
+        }
+    }
+}
+
+void inputMatrices(matrix *ms, int n_matrices) {
+    for (int i = 0; i < n_matrices; i++) {
+        printf("input %lld matrix\n", i + 1);
+        inputMatrix(ms + i);
+        printf("\n");
+    }
+}
+
+void outputMatrix(matrix *m) {
+    for (int i = 0; i < m->nRows; i++) {
+        for (int j = 0; j < m->nCols; j++) {
+            printf("%d ", m->values[i][j]);
+        }
+
+        printf("\n");
+    }
+    printf("\n");
+}
+
+void outputMatrices(matrix *ms, int nMatrices) {
+    for (int i = 0; i < nMatrices; i++) {
+        outputMatrix(ms + i);
+    }
+}
+
+void swapRows(matrix *m, int i1, int i2) {
+    int *temp = m->values[i1];
+    m->values[i1] = m->values[i2];
+    m->values[i2] = temp;
+}
+
+void swapColumns(matrix *m, int j1, int j2) {
+    for (int i = 0; i < m->nRows; i++) {
+        int temp = *(&m->values[i][j1]);
+        *(&m->values[i][j1]) = *(&m->values[i][j2]);
+        *(&m->values[i][j2]) = temp;
+    }
+}
+
+void insertionSortRowsMatrixByRowCriteria(matrix *m, int (*criteria)(int*, int)) {
+    int res_criteria[m->nRows];
+
+    for(int i = 0; i < m->nRows; i++)
+        res_criteria[i] = criteria(m->values[i], m->nCols);
+
+    for (int i = 1; i < m->nRows; i++) {
+        int key = res_criteria[i];
+        int *address_key = m->values[i];
+        int j = i - 1;
+
+        while (j >= 0 && res_criteria[j] > key) {
+            res_criteria[j + 1] = res_criteria[j];
+            swapRows(m, j + 1, j);
+
+            j -= 1;
+        }
+
+        res_criteria[j + 1] = key;
+        m->values[j + 1] = address_key;
+    }
+}
+
+void selectionSortColsMatrixByColCriteria(matrix *m, int (*criteria)(int*, int)) {
+    int res_criteria[m->nCols];
+
+    for (size_t i = 0; i < m->nCols; i++) {
+        int temp[m->nRows];
+        for (size_t j = 0; j < m->nRows; j++)
+            temp[j] = m->values[j][i];
+
+        res_criteria[i] = criteria(temp, m->nRows);
+    }
+
+    int value_min_idx;
+    for (int i = 0; i < m->nCols; i++) {
+        value_min_idx = i;
+
+        for (int j = i + 1; j < m->nCols; j++)
+            if (res_criteria[j] < res_criteria[value_min_idx])
+                value_min_idx = j;
+
+        int temp = *(&res_criteria[value_min_idx]);
+        *(&res_criteria[value_min_idx]) = *(&res_criteria[i]);
+        *(&res_criteria[i]) = temp;
+
+        swapColumns(m, value_min_idx, i);
+    }
+}
--- libs/data_structures/matrix/matrix.h
+++ libs/data_structures/matrix/matrix.h
@@ -14,17 +14,53 @@ typedef struct position {
     int colIndex;
 } position;
 
-// размещает в динамической памяти матрицу размером n_rows на n_cols. Возвращает матрицу.
-matrix getMemMatrix(int n_rows, int n_cols);
+// размещает в динамической памяти матрицу размером nRows на nCols. Возвращает матрицу.
+matrix getMemMatrix(int nRows, int nCols);
 
-// размещает в динамической памяти массив из n_matrices матриц размером n_rows на n_cols.
+// размещает в динамической памяти массив из nMatrices матриц размером nRows на nCols.
 // Возвращает указатель на нулевую матрицу.
-matrix *getMemArrayOfMatrices(int n_matrices, int n_rows, int n_cols);
+matrix *getMemArrayOfMatrices(int nMatrices, int nRows, int nCols);
 
 // освобождает память, выделенную под хранение матрицы m.
 void freeMemMatrix(matrix *m);
 
-// освобождает память, выделенную под хранение массива ms из n_matrices матриц.
-void freeMemMatrices(matrix *ms, int n_matrices);
+// освобождает память, выделенную под хранение массива ms из nMatrices матриц.
+void freeMemMatrices(matrix *ms, int nMatrices);
+
+// ввод матрицы m.
+void inputMatrix(matrix *m);
+
+// ввод массива из n_matrices матриц, хранящейся по адресу ms.
+void inputMatrices(matrix *ms, int nMatrices);
+
+// вывод матрицы m.
+void outputMatrix(matrix *m);
+
+// вывод массива из nMatrices матриц, хранящейся по адресу ms.
+void outputMatrices(matrix *ms, int nMatrices);
+
+// обмен строк с порядковыми номерами i1 и i2 в матрице m.
+void swapRows(matrix *m, int i1, int i2);
+
+//  обмен колонок с порядковыми номерами j1 и j2 в матрице m.
+void swapColumns(matrix *m, int j1, int j2);
+
+// выполняет сортировку вставками строк матрицы m по неубыванию значения функции criteria применяемой для строк.
+void insertionSortRowsMatrixByRowCriteria(matrix *m, int (*criteria)(int*, int));
+
+// выполняет сортировку выбором столбцов матрицы m по неубыванию значения функции criteria применяемой для столбцов.
+void selectionSortColsMatrixByColCriteria(matrix *m, int (*criteria)(int*, int));
+
+// возвращает значение ’истина’, если матрица m является квадратной, ложь – в противном случае.
+bool isSquareMatrix(matrix *m);
+
+// возвращает значение ’истина’, если матрицы m1 и m2 равны, ложь – в противном случае.
+bool areTwoMatricesEqual(matrix *m1, matrix *m2);
+
+// возвращает значение ’истина’, если матрица m является единичной, ложь – в противном случае.
+bool isEMatrix(matrix *m);
+
+// возвращает значение ’истина’, если матрица m является симметричной, ложь – в противном случае.
+bool isSymmetricMatrix(matrix *m);
 
 #endif //UNTITLED6_MATRIX_H
