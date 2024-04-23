92a55b81c2ceec614acf8d5d8bedd457850860b1
Abolmasow <zevssa1998@gmail.com>
2024-04-23 23:50:54 +0300

    adding sortRowsByMaxElement

--- main.c
+++ main.c
@@ -1,7 +1,20 @@
 #include "libs/data_structures/matrix/matrix.h"
+#include <assert.h>
 
 void swapMinMaxRows(matrix* m) {
     position max = getMaxValuePos(*m);
     position min = getMinValuePos(*m);
     swapRows(m, max.rowIndex, min.rowIndex);
+}
+
+int getMax(int *a, int n) {
+    int max = a[0];
+    for (size_t i = 1; i < n; i++)
+        if (a[i] > max)
+            max = a[i];
+    return max;
+}
+
+void sortRowsByMaxElement(matrix *m) {
+    insertionSortRowsMatrixByRowCriteria(m, getMax);
 }
