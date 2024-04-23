0a0f80054514c9976e97ba3824856e44009497fa
Abolmasow <zevssa1998@gmail.com>
2024-04-23 23:54:20 +0300

    adding sortColsByMinElement

--- main.c
+++ main.c
@@ -18,3 +18,15 @@ int getMax(int *a, int n) {
 void sortRowsByMaxElement(matrix *m) {
     insertionSortRowsMatrixByRowCriteria(m, getMax);
 }
+
+int getMin(int *a, int n) {
+    int min = a[0];
+    for (size_t i = 1; i < n; i++)
+        if (a[i] < min)
+            min = a[i];
+    return min;
+}
+
+void sortColsByMinElement(matrix *m) {
+    selectionSortColsMatrixByColCriteria(m, getMin);
+}
