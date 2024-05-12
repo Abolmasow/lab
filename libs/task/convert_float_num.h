0cda009106d9bace7ed6af318cc3f9a8fe00e5b6
Abolmasow <zevssa1998@gmail.com>
2024-05-12 21:28:55 +0300

    adding 2nd ex

---
+++ libs/task/convert_float_num.h
@@ -0,0 +1,133 @@
+#ifndef UNTITLED6_CONVERT_FLOAT_NUM_H
+#define UNTITLED6_CONVERT_FLOAT_NUM_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <assert.h>
+#include "../string/string_.h"
+#include "../data_structures/vectorVoid/vectorVoid.h"
+
+void generate_float_file(const char* filename, int num_numbers) {
+    FILE* file = fopen(filename, "w");
+
+    if (file == NULL) {
+        printf("Error opening file.\n");
+        return;
+    }
+
+    srand(time(NULL));
+    for (int i = 0; i < num_numbers; i++) {
+        float number = ((float)rand() / (float)(RAND_MAX)) * 100.0;
+        fprintf(file, "%.2f\n", number);
+    }
+
+    fclose(file);
+}
+
+void convert_float(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (file == NULL) {
+        printf("reading error\n");
+        exit(1);
+    }
+
+    vectorVoid v = createVectorVoid(0, sizeof(float));
+
+    while (!feof(file)) {
+        float x;
+        fscanf(file, "%f", &x);
+
+        pushBackV(&v, &x);
+    }
+
+    fclose(file);
+
+    file = fopen(filename, "w");
+
+    for (size_t i = 0; i < v.size; i++) {
+        float x;
+        getVectorValueV(&v, i, &x);
+        fprintf(file, "%.2f ", x);
+    }
+
+    deleteVectorV(&v);
+    fclose(file);
+}
+
+void test_convert_float_1_zero_quantity() {
+    const char filename[] = "C:\\Users\\zevss\\CLionProjects\\lab_work_19\\tasks_tests\\task_2_test_1.txt";
+
+    FILE* file = fopen(filename, "w");
+    fclose(file);
+
+    convert_float(filename);
+
+    file = fopen(filename, "r");
+
+    char data[10] = "";
+    fscanf(file, "%s", data);
+
+    fclose(file);
+
+    assert(strcmp_(data, "0.00") == 0);
+}
+
+void test_convert_float_2_one_element() {
+    const char filename[] = "C:\\Users\\zevss\\CLionProjects\\lab_work_19\\tasks_tests\\task_2_test_2.txt";
+
+    float f1 = 1.123123;
+
+    FILE* file = fopen(filename, "w");
+
+    fprintf(file, "%f", f1);
+
+    fclose(file);
+
+    convert_float(filename);
+
+    file = fopen(filename, "r");
+
+    char data[100] = "";
+    fgets(data, sizeof(data), file);
+
+    fclose(file);
+
+    char check[100] = "1.12 ";
+
+    assert(strcmp_(data, check) == 0);
+}
+
+void test_convert_float_3_more_element() {
+    const char filename[] = "C:\\Users\\zevss\\CLionProjects\\lab_work_19\\tasks_tests\\task_2_test_3.txt";
+
+    float f1 = 1.123123;
+    float f2 = 2.232323;
+    float f3 = 3.343434;
+    FILE* file = fopen(filename, "w");
+
+    fprintf(file, "%f %f %f", f1, f2,f3);
+
+    fclose(file);
+
+    convert_float(filename);
+
+    file = fopen(filename, "r");
+
+    char data[100] = "";
+    fgets(data, sizeof(data), file);
+
+    fclose(file);
+
+    char check[100] = "1.12 2.23 3.34 ";
+
+    assert(strcmp_(data, check) == 0);
+}
+
+void test_convert_float() {
+    test_convert_float_1_zero_quantity();
+    test_convert_float_2_one_element();
+    test_convert_float_3_more_element();
+}
+
+#endif //UNTITLED6_CONVERT_FLOAT_NUM_H
